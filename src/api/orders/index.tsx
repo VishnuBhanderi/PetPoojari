bbe2cbea9c34059864439ba6e4da29c63ab61b79
VishnuBhanderi <vishnubhanderi0712@gmail.com>
2024-02-12 20:27:26 +0530

    Read Order by Id

--- src/api/orders/index.tsx
+++ src/api/orders/index.tsx
@@ -42,3 +42,21 @@ export const useAdminOrderList = ({ archived = false }) => {
       },
     });
   };
+
+  export const useOrderDetails = (id: number) => {
+    return useQuery({
+      queryKey: ['orders', id],
+      queryFn: async () => {
+        const { data, error } = await supabase
+          .from('orders')
+          .select('*, order_items(*, products(*))')
+          .eq('id', id)
+          .single();
+  
+        if (error) {
+          throw new Error(error.message);
+        }
+        return data;
+      },
+    });
+  };
